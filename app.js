de801e5363064f0548bf90e1c6b4d3813885206c
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-07 16:30:18 +0500

    JavaScript | Class 35 | Firebase 🔥 | [Urdu/Hindi] Batch 11

--- app.js
+++ app.js
@@ -1,23 +1,92 @@
-var input = document.getElementById("todoInput");
+import {
+  doc,
+  setDoc,
+  db,
+  collection,
+  addDoc,
+  onSnapshot,
+  deleteDoc,
+  serverTimestamp ,
+  query, 
+  where,
+} from "./firebase.js";
+
+let todo = document.getElementById("todoInput");
 var list = document.getElementById("list");
 
-function add (){
-    if (input.value !== ""){ 
-        list.innerHTML +=  `<li>${input.value} <button id="crosbtn" onclick="remove(this)">   <img id="crosimg" src="https://clipart-library.com/images/gie5B478T.png" alt="Image Placeholder">
- 
-        </button></li>`;
-        input.value = "";
-    } else {
-        alert("Please enter a Task");
-    }
-}
+let add = document.getElementById("add");
 
-function remove(element){
-    element.parentNode.remove();
-} 
+let addfOO = async () => {
+  if (todo.value !== "") {
+    //         list.innerHTML +=  `<li>${todo.value} <button id="crosbtn" onclick="remove(this)">   <img id="crosimg" src="https://clipart-library.com/images/gie5B478T.png" alt="Image Placeholder">
+    //  </button></li>`;
 
-function clearAll() {
-    while (list.firstChild) {
-        list.removeChild(list.firstChild);
-    }
-}
+    let ref = collection(db, "todos");
+    await addDoc(ref, {
+      date: new Date().toLocaleString(),
+      todo: todo.value,
+      id: 1,
+      timestamp: serverTimestamp(),
+    });
+    console.log("todo added");
+    todo.value = "";
+  } else {
+    alert("Please enter a Task");
+  }
+};
+
+add && add.addEventListener("click", addfOO);
+
+let crosbtn = document.getElementById("crosbtn");
+
+// function remove(element){
+//     element.parentNode.remove();
+// }
+// crosbtn && crosbtn.addEventListener("click" , remove)
+
+// let clearbtn = document.getElementById("clearbtn");
+
+// function clearAll() {
+//     while (list.firstChild) {
+//         list.removeChild(list.firstChild);
+//     }
+// }
+
+// clearbtn && clearbtn.addEventListener("click", clearAll);
+
+let getTodos = () => {
+let q = query(collection(db, "todos") , where("id", "<=",  10));
+
+  onSnapshot (q ,(Snapshot) => {
+    list.innerHTML = "";
+    Snapshot.docChanges().forEach((changes) => {
+      console.log("🚀 ~ Snapshot.docChanges ~ changes:", changes.type);
+    });
+
+    Snapshot.forEach((doc) => {
+      let { todo } = doc.data();
+
+      list.innerHTML += `<li>${todo} <button id="crosbtn" onclick="delTodo('${doc.id}')">   <img id="crosimg" src="https://clipart-library.com/images/gie5B478T.png" alt="Image"> </button></li>`;
+    });
+  });
+};
+getTodos();
+
+let delTodo = async (id) => {
+  console.log("🚀 ~ delTodo ~ id:", id);
+  await deleteDoc(doc(db, "todos", id));
+  console.log("Todo Deleted");
+};
+
+window.delTodo = delTodo;
+
+// let getTodos = async () => {
+//     const ref = collection(db, "todos");
+//     onSnapshot(ref, (doc) => {
+//         doc.forEach((doc) => {
+//         list.innerHTML += `<li>${doc.data().todo} <button id="crosbtn" onclick="remove(this)">   <img id="crosimg" src="https://clipart-library.com/images/gie5B478T.png" alt="Image">
+//         </button></li>`;
+//         })
+//     })
+// }
+// getTodos()
---
+++ firebase.js
@@ -0,0 +1,39 @@
+
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import {} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+
+// firestore
+
+import { doc, setDoc,getFirestore ,collection, addDoc,onSnapshot,deleteDoc ,serverTimestamp, query, where,   } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAFCfzc0RM7pWN54L79Q5mX4a2snu3VmA",
+  authDomain: "extended-legend-415308.firebaseapp.com",
+  projectId: "extended-legend-415308",
+  storageBucket: "extended-legend-415308.appspot.com",
+  messagingSenderId: "769368000323",
+  appId: "1:769368000323:web:186f4630f1d4118f6cd1f6",
+  measurementId: "G-FMMPHYKG3Y",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {
+  doc, 
+  setDoc,
+  db,
+  collection,
+   addDoc,
+   onSnapshot,
+   deleteDoc ,
+   serverTimestamp ,
+   query,
+    where,
+};
